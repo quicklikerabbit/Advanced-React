782dd41b30dc83da89c9265360c99db8b55adb1b
Steven Richards <steve@syncline.ca>
2018-10-29 11:04:59 -0700

    Module 1 complete

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hey you!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Sell = () => {
+  return (
+    <div>
+      <p>Sell!</p>
+    </div>
+  );
+};
+
+export default Sell;
