42843f660e92cb9502bb1e36b6e9315b17a0c453
Steven Richards <steve@syncline.ca>
2018-11-13 22:54:23 -0700

    create permissions table

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -9,10 +9,20 @@ const server = createServer();
 server.express.use(cookieParser());
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
-  if (token ) {
+  if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     req.userId = userId;
   }
+  next();
+});
+
+server.express.use(async (req, res, next) => {
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{id, email, permissions, name}"
+  );
+  req.user = user;
   next();
 });
 
